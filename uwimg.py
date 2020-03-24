297ff52e71093f5c3659a6361d2c72c24f0dd4fc
Bindita Chaudhuri <bindita@cs.washington.edu>
2020-03-23 21:33:28 -0700

    Add files via upload

--- uwimg.py
+++ uwimg.py
@@ -54,7 +54,6 @@ class MODEL(Structure):
 
 (LINEAR, LOGISTIC, RELU, LRELU, SOFTMAX) = range(5)
 
-
 make_image = lib.make_image
 make_image.argtypes = [c_int, c_int, c_int]
 make_image.restype = IMAGE
@@ -118,6 +117,62 @@ def save_image(im, f):
 same_image = lib.same_image
 same_image.argtypes = [IMAGE, IMAGE]
 same_image.restype = c_int
+
+##### HOMEWORK 1
+
+nn_resize = lib.nn_resize
+nn_resize.argtypes = [IMAGE, c_int, c_int]
+nn_resize.restype = IMAGE
+
+bilinear_resize = lib.bilinear_resize
+bilinear_resize.argtypes = [IMAGE, c_int, c_int]
+bilinear_resize.restype = IMAGE
+
+make_box_filter = lib.make_box_filter
+make_box_filter.argtypes = [c_int]
+make_box_filter.restype = IMAGE
+
+##### HOMEWORK 2
+
+make_highpass_filter = lib.make_highpass_filter
+make_highpass_filter.argtypes = []
+make_highpass_filter.restype = IMAGE
+
+make_emboss_filter = lib.make_emboss_filter
+make_emboss_filter.argtypes = []
+make_emboss_filter.restype = IMAGE
+
+make_sharpen_filter = lib.make_sharpen_filter
+make_sharpen_filter.argtypes = []
+make_sharpen_filter.restype = IMAGE
+
+add_image = lib.add_image
+add_image.argtypes = [IMAGE, IMAGE]
+add_image.restype = IMAGE
+
+sub_image = lib.sub_image
+sub_image.argtypes = [IMAGE, IMAGE]
+sub_image.restype = IMAGE
+
+make_gy_filter = lib.make_gy_filter
+make_gy_filter.argtypes = []
+make_gy_filter.restype = IMAGE
+
+make_gx_filter = lib.make_gx_filter
+make_gx_filter.argtypes = []
+make_gx_filter.restype = IMAGE
+
+sobel_image = lib.sobel_image
+sobel_image.argtypes = [IMAGE]
+sobel_image.restype = POINTER(IMAGE)
+
+make_gaussian_filter = lib.make_gaussian_filter
+make_gaussian_filter.argtypes = [c_float]
+make_gaussian_filter.restype = IMAGE
+
+convolve_image = lib.convolve_image
+convolve_image.argtypes = [IMAGE, IMAGE, c_int]
+convolve_image.restype = IMAGE
 
 if __name__ == "__main__":
     im = load_image("data/dog.jpg")
