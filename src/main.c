e1cddeb0d70a7ee1a697676943f5ad14405c9664
Bindita Chaudhuri <bindita@cs.washington.edu>
2020-03-23 20:32:36 -0700

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -10,8 +10,8 @@ int main(int argc, char **argv)
         printf("usage: %s test <hw0 | hw1...>\n", argv[0]);  
     } else if (0 == strcmp(argv[1], "test")){
         if (0 == strcmp(argv[2], "hw0")) test_hw0();
-        if (0 == strcmp(argv[2], "hw1")) test_hw1();
         /*
+        if (0 == strcmp(argv[2], "hw1")) test_hw1();
         if (0 == strcmp(argv[2], "hw2")) test_hw2();
         if (0 == strcmp(argv[2], "hw3")) test_hw3();
         if (0 == strcmp(argv[2], "hw4")) test_hw4();
--- src/test.h
+++ src/test.h
@@ -9,8 +9,8 @@ extern int tests_fail;
     ++tests_fail; }} while (0)
 
 void test_hw0();
-void test_hw1();
 /*
+void test_hw1();
 void test_hw2();
 void test_hw3();
 void test_hw4();
